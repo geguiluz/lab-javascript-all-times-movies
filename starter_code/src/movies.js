240c7362429bace6389a324997e2a5ae5a06b494
geguiluz <memo.eguiluz@gmail.com>
2019-05-04 14:54:00 -0500

    Algoritmo listo pero sin funcionar (1er ejercicio)

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,5 +1,37 @@
+//Check valid arrays 
+function checkValidArray (array) {
+  if (array === undefined) {
+    // console.log("El arreglo es incorrecto");
+    return false;
+  } else if (array.length === 0) {
+    // console.log("El arreglo está vacío");
+    return false;
+  } else {
+    // console.log("El arreglo es correcto");
+    return true;
+  }
+ }
+
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
+
+let minuteConverter = durationString => {
+  // console.log("Raw duration:", durationString);
+  // console.log("String W/O spaces",durationString.replace(/ /g,''));
+  // console.log("String separated",durationString.split(" ", 3));
+  timeArray = durationString.split(" ", 3);
+  // console.log(timeArray);
+  hours = timeArray[0].replace('h','');
+  minutes = timeArray[1].replace('min','');
+  totalTime = hours * 60 + parseInt(minutes);
+  console.log(totalTime);
+  return totalTime;
+}
+
+let turnHoursToMinutes = movieArray => movieArray.map(elem => {
+  // checkValidArray(movieArray);
+  minuteConverter(elem.duration);
+});
 
 
 // Get the average of all rates with 2 decimals 
